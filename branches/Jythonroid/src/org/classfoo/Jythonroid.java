835c7f1c455da2d14ff3ebeb57174a5121289ff3
juanzhewudi <juanzhewudi@b325f846-1f4a-0410-9876-7ba70d661821>
2008-12-06 06:28:50 +0000

    so far the Package.getPackages method is not use in other place else than JavaImporHelper.java,and the stringmap got from the Package class was only used to check whether peticular package was loaded to ignore overload, so simple make the isLoadedPackage return false will fix the from package import * bug

--- branches/Jythonroid/src/org/classfoo/Jythonroid.java
+++ branches/Jythonroid/src/org/classfoo/Jythonroid.java
@@ -1,24 +1,16 @@
 package org.classfoo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
+import org.python.util.ClassFoo;
 import org.python.util.PythonInterpreter;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.text.Editable;
 import android.text.Selection;
 import android.view.KeyEvent;
@@ -30,105 +22,22 @@ import android.view.View.OnFocusChangeListener;
 import android.view.View.OnKeyListener;
 import android.view.View.OnLongClickListener;
 import android.widget.EditText;
+
 /**
  * the gui part of jythonroid<br>
  * it will only contains a easy console of python<br>
+ * 
  * @author fuzhiqin
  * 
  */
 public class Jythonroid extends Activity {
-//	ShellClient shellclient = null;
-
+	// ShellClient shellclient = null;
+	private PythonInterpreter pi = null;
 	private String ps1 = ">>> ";
 
 	private String ps2 = "+++ ";
 
 	private EditText shell = null;
-
-	// private ShellEditer shell=null;
-
-//	class ShellClient implements Runnable {
-//		private String address = "127.0.0.1";
-//
-//		private int port = 6000;
-//
-//		private InputStream is = null;
-//
-//		private OutputStream os = null;
-//
-//		private boolean keeponrun = true;
-//
-//		private Jythonroid jr = null;
-//
-//		private BufferedReader br = null;
-//
-//		private PrintWriter pw = null;
-//
-//		private Handler hd = null;
-//
-//		public ShellClient(Jythonroid jythonroid, Handler hd) {
-//			this.jr = jythonroid;
-//			this.hd = hd;
-//			// wait till connected to the server
-//			Socket s = null;
-//			do {
-//				try {
-//					s = new Socket(address, port);
-//					this.wait(1000);
-//				} catch (UnknownHostException e1) {
-//				} catch (IOException e1) {
-//				}
-//				// FIXME sending some message here
-//				// callHandler();
-//				catch (InterruptedException e) {
-//				}
-//			} while (s == null);
-//			callHandler("initial", true);
-//			try {
-//				is = s.getInputStream();
-//				os = s.getOutputStream();
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			br = new BufferedReader(new InputStreamReader(is));
-//			pw = new PrintWriter(new OutputStreamWriter(os));
-//			// sendMsg("2");
-//		}
-//
-//		public void run() {
-//			// is initialized
-//			try {
-//				while (keeponrun) {
-//					String result = br.readLine();
-//					callHandler("result", result);
-//				}
-//			} catch (IOException e) {
-//			}
-//		}
-//
-//		private void callHandler(String key, String str) {
-//			Message msg = Message.obtain();
-//			Bundle data = new Bundle();
-//			data.putString(key, str);
-//			msg.setData(data);
-//			hd.sendMessage(msg);
-//		}
-//
-//		private void callHandler(String key, Boolean boo) {
-//			Message msg = Message.obtain();
-//			Bundle data = new Bundle();
-//			data.putBoolean(key, boo);
-//			// data.put(key, boo);
-//			msg.setData(data);
-//			hd.sendMessage(msg);
-//		}
-//
-//		public void sendMsg(String msg) {
-//			pw.println(msg);
-//			pw.flush();
-//		}
-//	}
 
 	/**
 	 * provide an interactive shell in the screen
@@ -139,40 +48,9 @@ public class Jythonroid extends Activity {
 		setContentView(R.layout.main);
 		shell = (EditText) findViewById(R.id.shell);
 		// shell=new ShellEditer(this, null, null);
-		//setContentView(shell);
+		// setContentView(shell);
 		shell.setEnabled(true);
 		initializeShell(shell);
-//		Handler hd = new Handler() {
-//			public void handleMessage(Message msg) {
-//				if (msg.getData().containsKey("initial")) {
-//					alert("initialized");
-//					shell.setEnabled(true);
-//				} else {
-//					shell.append("\n" + (String) msg.getData().get("result"));
-//				}
-//			}
-//		};
-		// running the backend
-//		new Thread(new Runnable() {
-//			public void run() {
-//				try {
-//					Runtime
-//							.getRuntime()
-//							.exec(
-//									"dalvikvm -classpath "
-//											+ "/data/app/org.classfoo.apk "
-//											+ "org.python.util.JythonServer");
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}, "JythonServer").start();
-//		shellclient = new ShellClient(this, hd);
-//		new Thread(shellclient).start();
-	}
-
-	public void println(String line) {
-
 	}
 
 	/**
@@ -191,6 +69,7 @@ public class Jythonroid extends Activity {
 	private int deep = 0;
 
 	private void initializeShell(EditText shell) {
+
 		final StringBuffer sb = new StringBuffer();
 		// FIXME set the cursor to the end of '>>>'
 		// cursorToEnd(shell);
@@ -202,18 +81,39 @@ public class Jythonroid extends Activity {
 				EditText shell = (EditText) v;
 				switch (i) {
 				case 66:
-//					if (k.getAction() == 1) {
-						shell.setSelection(shell.getText().length());
-						String[] f = shell.getText().toString().split("\n");
-						String msg = f[f.length - 1].substring(3);
-						PythonInterpreter pi=new PythonInterpreter();
-						StringWriter sw=new StringWriter();
-						pi.setOut(sw);
-						pi.exec(msg);
-						alert(sw.toString());
-						
-//						shellclient.sendMsg(msg);
+					alert(new ClassFoo().getStr());
+					
+					// if (k.getAction() == 1) {
+					// initialize the PythonInterpreter
+//					new Thread(){
+//						public void run(){
+//							PythonInterpreter pi=new PythonInterpreter();	
+//							alert(pi.toString());
+//						}
+//					}.start();
+
+//					try {
+//						alert("a");
+//						DexFile f=new DexFile(new File("/data/app/org.classfoo.apk"));
+//						alert("b");
+//						Class<?> c = f.loadClass("org/python/util/PythonInterpreter", System.class.getClassLoader());
+//						alert("c");
+//						PythonInterpreter pi = (PythonInterpreter) c.newInstance();
+//						alert("d'");
+//
+//					} catch (IllegalAccessException e) {
+//						alert(e.toString());
+//					} catch (InstantiationException e) {
+//						alert(e.toString());
+//					}catch (IOException e) {
+//						alert(e.toString());
 //					}
+					shell.setSelection(shell.getText().length());
+					String[] f = shell.getText().toString().split("\n");
+					String msg = f[f.length - 1].substring(3);
+					// pi.exec(msg);
+
+					alert("执行完毕");
 					return true;
 				case 19:
 					alert("19");
@@ -302,4 +202,16 @@ public class Jythonroid extends Activity {
 						}).setCancelable(true).setMessage("Hey").show();
 	}
 
+	class foo extends URLClassLoader {
+		public foo(String path) throws MalformedURLException {
+			super(new URL[] { new URL(path) });
+		}
+
+		@Override
+		protected Class<?> findClass(String className)
+				throws ClassNotFoundException {
+			alert("查找类");
+			return super.findClass(className);
+		}
+	}
 }
